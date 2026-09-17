f9cdc53c8569646282d907b47bb6f6d41f74a2bf
Aden Abdi <adenyyussufabdi94@gmail.com>
2026-09-17 15:21:13 +0300

    add all about me program

---
+++ all_about_me.py
@@ -0,0 +1,11 @@
+# This program introduces me and my goals for the course.
+
+name = "Aden"
+age = 20
+favourite_language = "Python"
+goal = "To become a skilled Python programmer"
+
+print("My name is", name)
+print("I am", age, "years old.")
+print("My favourite programming language is", favourite_language)
+print("My goal for this course is", goal)
